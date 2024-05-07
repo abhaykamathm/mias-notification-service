9fcf7bcadd71397d702c7ed517795c8ee2e80084
Abhay Kamath <abhay.kamath@gaiansolutions.com>
2024-05-07 12:00:22 +0530

    Added functionality to emit Museo notifications in addition to regular notifications upon server startup

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const server = http.createServer(app);
 const io = socketIO(server, { cors: { origin: "*" } });
 
 const messages = require("./messages.json");
+const museo_notifications = require("./data/museo_notifications.json");
 const PORT = process.env.PORT || 4000;
 
 const formatDateTime = (date) => {
@@ -40,9 +41,27 @@ const emitMessages = () => {
   }, 5000); // Change interval as needed
 };
 
+const emitMuseoNotifications = () => {
+  let index = 0;
+
+  setInterval(() => {
+    const currentDate = new Date();
+    const formattedDateTime = formatDateTime(currentDate);
+    const messageWithDateTime = {
+      message: museo_notifications[index].message,
+      type: museo_notifications[index].type,
+      dateTime: formattedDateTime,
+    };
+    io.emit("museo_notification", messageWithDateTime);
+    console.log("Emitted");
+    index = (index + 1) % museo_notifications.length;
+  }, 5000); // Change interval as needed
+};
+
 app.use(cors()); // Enable CORS for all routes
 
 server.listen(PORT, () => {
   emitMessages();
+  emitMuseoNotifications();
   console.log(`Server listening on port ${PORT}`);
 });
--- notification-service-ui/src/App.jsx
+++ notification-service-ui/src/App.jsx
@@ -2,11 +2,15 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-// const socket = io("http://localhost:4000");
-const socket = io("https://mias-notification-service.onrender.com");
+const socket = io("http://localhost:4000");
+// const socket = io("https://mias-notification-service.onrender.com");
 
 function App() {
   const [notification, setNotification] = useState({
+    message: "Hello! This is a notification message.",
+    dateTime: "6 May 2024, 16:12:58",
+  });
+  const [museoNotification, setMuseoNotification] = useState({
     message: "Hello! This is a notification message.",
     dateTime: "6 May 2024, 16:12:58",
   });
@@ -16,6 +20,11 @@ function App() {
     socket.on("notification", (data) => {
       // console.log(data);
       setNotification((prevNotification) => data);
+    });
+
+    socket.on("museo_notification", (data) => {
+      // console.log(data);
+      setMuseoNotification((prevNotification) => data);
     });
 
     return () => {
@@ -31,28 +40,34 @@ function App() {
       </nav>
       {/* Notifications Grid */}
       <div className="p-[1vw] grid grid-cols-3 gap-[1vw]">
+        {/* Aegis Card */}
         <div className="p-[1vw] flex flex-col gap-[0.5vw] rounded-[1vw] bg-[#76885B]">
           <span className="text-[white] text-[2vw] font-[500]">Aegis</span>
           <div className="p-[0.5vw] flex flex-col gap-[1vw] bg-[white] rounded-[0.75vw]">
-            <div className="text-[1.5vw]">{notification.message}</div>
+            <div className="text-[1.5vw]">Message : {notification.message}</div>
             <span className="text-[1vw] font-[500] text-[#ab5037]">
               {notification.dateTime}
             </span>
           </div>
         </div>
+        {/* Museo Card */}
         <div className="p-[1vw] flex flex-col gap-[0.5vw] rounded-[1vw] bg-[#76885B]">
           <span className="text-[white] text-[2vw] font-[500]">Museo</span>
           <div className="p-[0.5vw] flex flex-col gap-[1vw] bg-[white] rounded-[0.75vw]">
-            <div className="text-[1.5vw]">{notification.message}</div>
+            <div className="text-[1.5vw]">
+              Message : {museoNotification.message}
+            </div>
+            <div className="text-[1.5vw]">Type : {museoNotification.type}</div>
             <span className="text-[1vw] font-[500] text-[#ab5037]">
-              {notification.dateTime}
+              {museoNotification.dateTime}
             </span>
           </div>
         </div>
+        {/* iZAK Card */}
         <div className="p-[1vw] flex flex-col gap-[0.5vw] rounded-[1vw] bg-[#76885B]">
           <span className="text-[white] text-[2vw] font-[500]">iZAK</span>
           <div className="p-[0.5vw] flex flex-col gap-[1vw] bg-[white] rounded-[0.75vw]">
-            <div className="text-[1.5vw]">{notification.message}</div>
+            <div className="text-[1.5vw]">Message : {notification.message}</div>
             <span className="text-[1vw] font-[500] text-[#ab5037]">
               {notification.dateTime}
             </span>
